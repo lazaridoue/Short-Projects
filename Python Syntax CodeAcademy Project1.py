63d861ffc369892f67dccc25ecb455ed6b6dccd1
lazaridoue <lazaridoue@berea.edu>
2020-04-01 01:33:58 -0400

    Added Comments

--- Python Syntax CodeAcademy Project1.py
+++ Python Syntax CodeAcademy Project1.py
@@ -1,3 +1,6 @@
+# CodeAcademy Project
+#Create Purchasing Information and Receipts for Lovely Loveseats
+
 lovely_loveseat_description = "Lovely Loveseat. Tufted polyester blend on wood. 32 inches high x 40 inches wide x 30 inches deep. Red or white."
 lovely_loveseat_price = 254.00
 stylish_settee_description = """Stylish Settee. Faux leather on birch. 29.50 inches high x 54.75 inches wide x 28 inches deep. Black."""
@@ -11,7 +14,10 @@ customer_one_total += lovely_loveseat_price+luxurious_lamp_price
 customer_one_itemization += lovely_loveseat_description+luxurious_lamp_description
 customer_one_tax = customer_one_total*sales_tax
 customer_one_total += customer_one_tax
-print("Customer One Items:" + customer_one_itemization)
 
+#printing the items and the total
+print("Customer One Items:" + customer_one_itemization)
 print("Customer One Total: $" + str(int(customer_one_total)))
+
+
 
